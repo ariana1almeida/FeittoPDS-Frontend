992810eebc19ba9a4f918a2258b842e01e8170e1
ariana1almeida <arianaleu@hotmail.com>
2025-12-09 19:41:58 -0300

    fix- build

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 export const api = axios.create({
-   baseURL: "http://localhost:3000",
-  //baseURL: "https://feittopds-backend.onrender.com",
+   // baseURL: "http://localhost:3000",
+  baseURL: "https://feittopds-backend.onrender.com",
   headers: { "Content-Type": "application/json" },
 });
 
