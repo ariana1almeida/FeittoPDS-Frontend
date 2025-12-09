d8faa593e48da4edba46871cbdb037803a1221cb
ariana1almeida <arianaleu@hotmail.com>
2025-12-09 05:41:59 -0300

    refactor - altera URL base para bater no serviço do render e não local

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:3000",
+  // baseURL: "http://localhost:3000",
+  baseURL: "https://feittopds-backend.onrender.com",
   headers: { "Content-Type": "application/json" },
 });
 
